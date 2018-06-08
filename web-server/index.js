bdddaa4816921b38da970c191a3d8266efde1282
Justin Morris <justinjodymorris@gmail.com>
2018-06-08 15:05:18 -0700

    changed web server to account for heroku or localhost route

--- web-server/index.js
+++ web-server/index.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+
+require('dotenv').config();
+
+const port = process.env.PORT;
 
 app.use(express.static('./public'));
 
-app.listen(PORT, () => {
-  console.log('Web Server up on port', PORT);
+app.listen(port, () => {
+  console.log('Web Server up on port', port);
 });
