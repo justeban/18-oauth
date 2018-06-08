7434d34376816f78101b946d79ed78c459e0b281
Justin Morris <justinjodymorris@gmail.com>
2018-06-08 14:38:30 -0700

    changed port to run on Heroku

--- web-server/index.js
+++ web-server/index.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.static('./public'));
 
 app.listen(PORT, () => {
-  console.log('Web Server up on port', process.env.PORT);
+  console.log('Web Server up on port', PORT);
 });
